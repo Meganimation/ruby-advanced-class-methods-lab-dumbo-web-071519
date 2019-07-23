a5766c2e43fa71d7b1ad4ac42e8c0fafe56f7eaf
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-23 01:20:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,24 +9,5 @@ class Song
   def save
     self.class.all << self
   end
-  
-    def self.create
-      song = Song.new
-      song.save 
-      song
-end
-
-def self.new_by_name(song_name)
-  song = self.new
-  song.name = song_name 
-  song
-end
-
-def self.create_by_name(song_name)
-  song = self.create
-  song.name = song_name 
-  song
-end
-
 
 end
